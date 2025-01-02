39867b5f0a8b9be919112f00664df4ed31d61c34
Miguel Castorena <miguel.angel.castorena@gmail.com>
2025-01-02 14:13:27 -0800

    Init I2C configurable items initialization values

--- drivers/Inc/stm32f407xx_i2c_driver.h
+++ drivers/Inc/stm32f407xx_i2c_driver.h
@@ -30,4 +30,22 @@ typedef struct
 	I2C_Config_t I2C_Config;
 }I2C_Handle_t;
 
+/**
+ * @I2C_SCLSpeed
+ */
+#define I2C_SCL_SPEED_SM		100000		// Standard mode, 100KHz
+#define I2C_SCL_SPEED_FM2K		200000		// Fast mode, 200KHz
+#define I2C_SCL_SPEED_FM4K		400000		// Fast mode, 400KHz
+
+/**
+ * @I2C_ACKControl
+ */
+#define I2C_ACK_ENABLE			1
+#define I2C_ACK_DISABLE			0
+
+/**
+ * @I2C_FMDutyCycle
+ */
+#define I2C_FM_DUTY_2			0
+#define I2C_FM_DUTY_16_9		1
 #endif /* INC_STM32F407XX_I2C_DRIVER_H_ */
